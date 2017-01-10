fc995253da67306fd774e0209e57589bd5e4cce5
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-01-11 00:00:19 +0100

    Fixed format for same month/year test pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-export default function humanDateRange(startDate, endDate) {
-  return '';
+export default function humanDateRange({
+  startDate,
+  endDate,
+  showWeekDays = false,
+  month = 'short',
+  year = 'numeric',
+  weekday = 'short'
+}) {
+  // Make sure both startDate and endDate are dates
+  if (!isDate(startDate) || !isDate(endDate)) {
+    return null;
+  }
+
+  console.log('startDate', startDate);
+
+  const sY = startDate.getFullYear();
+  const sM = startDate.getMonth();
+  const sD = startDate.getDate();
+  const eY = endDate.getFullYear();
+  const eM = endDate.getMonth();
+  const eD = endDate.getDate();
+
+  // Check if month and year are the same
+  if (sY === eY && sM === eM) {
+    return `${startDate.toLocaleString('en-US', {month})} ${sD}-${eD} ${sY}`;
+  }
+
+  return null;
+}
+
+function isDate(date) {
+  return Object.prototype.toString.call(date) === '[object Date]';
 }
--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,14 @@ import humanDateRange from './index';
 
 describe('humanDateRange', () => {
   it('should recognize dates within the same month', () => {
-    expect(humanDateRange(new Date(2017, 1, 1), new Date(2017, 1, 6))).toBe('Jan 1-6 2017');
+    expect(humanDateRange({startDate: new Date(2017, 0, 1), endDate: new Date(2017, 0, 6)})).toBe('Jan 1-6 2017');
+  });
+
+  it('should recognize dates within the same year', () => {
+    expect(humanDateRange({startDate: new Date(2017, 0, 1), endDate: new Date(2017, 1, 1)})).toBe('Jan 1-Feb 1 2017');
+  });
+
+  it('should print full dates when different years', () => {
+    expect(humanDateRange({startDate: new Date(2016, 0, 1), endDate: new Date(2017, 0, 1)})).toBe('Jan 1 2016-Jan 1 2017');
   });
 });
